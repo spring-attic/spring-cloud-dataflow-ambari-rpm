4a1f536f289caf20a4de7d22577a9df0ced65bb2
Janne Valkealahti <janne.valkealahti@gmail.com>
2016-04-22 09:30:26 +0100

    Add support for HDP 2.4
    - Fixes #2

--- src/main/package/rpm/postinstall.sh
+++ src/main/package/rpm/postinstall.sh
@@ -49,7 +49,16 @@ if os.path.exists('/var/lib/ambari-server/resources/stacks/HDP/2.3/role_command_
   data['general_deps']['SCDFSERVER-START'] = ['SCDFH2-START','ZOOKEEPER_SERVER-START','KAFKA_BROKER-START','NODEMANAGER-START','RESOURCEMANAGER-START']
   json_data.seek(0)
   json.dump(data, json_data, indent=2)
-  json_data.close() 
+  json_data.close()
+
+if os.path.exists('/var/lib/ambari-server/resources/stacks/HDP/2.4/role_command_order.json'):
+  json_data=open('/var/lib/ambari-server/resources/stacks/HDP/2.4/role_command_order.json', 'r+')
+  data = json.load(json_data)
+  data['general_deps']['SCDF-INSTALL'] = ['HDFS-INSTALL']
+  data['general_deps']['SCDFSERVER-START'] = ['SCDFH2-START','ZOOKEEPER_SERVER-START','KAFKA_BROKER-START','NODEMANAGER-START','RESOURCEMANAGER-START']
+  json_data.seek(0)
+  json.dump(data, json_data, indent=2)
+  json_data.close()
 
 if os.path.exists('/var/lib/ambari-server/resources/stacks/PHD/3.0/repos/repoinfo.xml'):
   updateRepoWithScdf('/var/lib/ambari-server/resources/stacks/PHD/3.0/repos/repoinfo.xml')
@@ -59,6 +68,9 @@ if os.path.exists('/var/lib/ambari-server/resources/stacks/HDP/2.2/repos/repoinf
 
 if os.path.exists('/var/lib/ambari-server/resources/stacks/HDP/2.3/repos/repoinfo.xml'):
   updateRepoWithScdf('/var/lib/ambari-server/resources/stacks/HDP/2.3/repos/repoinfo.xml')
+
+if os.path.exists('/var/lib/ambari-server/resources/stacks/HDP/2.4/repos/repoinfo.xml'):
+  updateRepoWithScdf('/var/lib/ambari-server/resources/stacks/HDP/2.4/repos/repoinfo.xml')
 
 EOT
 
